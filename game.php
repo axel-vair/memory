60f605f2e18791b1a9393265465c2e2ea57b7685
Axel VAIR <axelvair@MacBook-Pro-de-Axel.local>
2023-01-15 13:45:01 +0100

    ajout du compare et de la condition 2 cartes

--- game.php
+++ game.php
@@ -11,23 +11,29 @@ $board = new Game();
 $cards = $board->board();
 $_SESSION['cards'] = !isset($_SESSION['cards']) ? $cards : $_SESSION['cards']; // Si session card n'est pas isset alors = $cards, sinon = $_SESSION
 
+$numberOfCardsTurned = 0;
 foreach($_SESSION['cards'] as $key => $card) { // boucle pour l'état : si get id == l'idée de la carte cliquée alors on passe à true
     $id = $_GET['id'] ?? null;
-    if($_GET['id'] == $card->getIdCard()) {
-        $card->setStates(true);
-        $_SESSION['compare'][] = $card;
-        echo "<pre>";
-        var_dump('card', $card);
-        echo "</pre>";
 
-        echo "<pre>";
-        var_dump('compare', $_SESSION['compare']);
-        echo "</pre>";
+    if($_GET['id'] == $card->getIdCard()) { // si get id et la l'id de la carte correspondent
+        if($numberOfCardsTurned < 2) { // si carte returned est inférieur à 2
+            $card->setStates(true); // alors on passe l'état de la carte à true
+            $_SESSION['compare'][] = $card; //on stocke la carte dans une session compare
+            $numberOfCardsTurned++; // on incrémente le compteur de carte
+        }else{
+            echo "Seules deux cartes peuvent être retournées simultanément"; // sinon on envoie un message (qui ne s'affichera jamais)
+        }
 
-        if(isset($_SESSION['compare']) && count($_SESSION['compare']) > 1){
-            unset($_SESSION['compare']);
+        if(isset($_SESSION['compare']) && count($_SESSION['compare']) > 1){ // si compare est isset et que son count est supérieur à 1
+            if($_SESSION['compare'][0]->getFace() != $_SESSION['compare'][1]->getFace()){ // si l'index 0 de compare est différent de l'index 1
+                $_SESSION['compare'][0]->setStates(false); //on repasse les états à false pour afficher le back
+                $_SESSION['compare'][1]->setStates(false);
+            }
+            unset($_SESSION['compare']); // on vide la tableau
+            $numberOfCardsTurned = 0;
         }
     }
+
 }
 
 
